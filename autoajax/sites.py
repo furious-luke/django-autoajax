6cd6ffaa0114fcdfd885aecc9bd0f393dc828f8b
Luke Hodkinson <furious.luke@gmail.coim>
2015-03-23 21:58:19 +1100

    I had hoped that handling whole site prefixes could be done with reverse_lazy, but it doesn't work as expected. Instead I have to use a far less elegant solution; specifying a prefix as needed.

--- autoajax/sites.py
+++ autoajax/sites.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.conf import settings
 from .forms import DependentSelect
 
 class AutoAjaxSite(object):
@@ -8,11 +9,13 @@ class AutoAjaxSite(object):
         self._urls = None
 
     def make_urls(self):
+        prefix = settings.AUTOAJAX_PREFIX if hasattr(settings, 'AUTOAJAX_PREFIX') else None
         urls = []
         for app, form_cls, field_name, field in self._registry:
             form_name = form_cls.__name__.lower()
             path = 'autoajax/' + '/'.join([app.name, form_name, field_name])
             urls.append(url('^%s$'%path, field.as_view()))
+            path = ((prefix + '/') if (prefix is not None) else '') + path
             field.widget.attrs['data-url'] = '/' + path
         self._urls = urls
 
